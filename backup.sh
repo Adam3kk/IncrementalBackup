b94a944a752ebede0fd0f4b1f1018c2344ee04ae
Adam <adam.walkowiak028@gmail.com>
2024-06-01 18:47:50 +0200

    The initial version of the function for create incremental backups and add a function to check the snapshot.file.

--- backup.sh
+++ backup.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
+#Konfiguracja
 DIRS=("/home/adam/files" "/test/files") #sciezki do okreslonych folderow
-BACKUP_DIR="/tmp/backup"
-TIMESTAMP=$(date +%Y_%m_%d_%H:%M:%S) #aktualna data
+BACKUP_DIR="/tmp/backup" #sciezka do zapisywania kopii
+TIMESTAMP=$(date +%Y_%m_%d_%H_%M_%S) #aktualna data
+SNAPSHOT_FILE="$BACKUP_DIR/snapshot.file" #plik pamietajacy ostatnie zmiany w kopiach
 
 
+check_snapshot_file(){
+	if [ ! -f "$SNAPSHOT_FILE" ]; then
+		touch $SNAPSHOT_FILE
+		echo "Utworzono plik snapshot.file"
+	else
+		echo "Plik snapshot.file juz istnieje"
+	fi
+}
 
 #funkcja do tworzenia kopii przyrostowej
 create_backup(){
 	local backup_name="backup_$TIMESTAMP"
-	local temp_backup="$BACKUP_DIR/$backup_name"
-	mkdir -p $temp_backup 
+	local temp_backup="$BACKUP_DIR/$backup_name.tar"
 	
-
-
+	tar --listed-incremental=$SNAPSHOT_FILE -czf $temp_backup ${DIRS[@]}
 }
 
 
@@ -34,7 +42,6 @@ for dir in ${DIRS[@]}; do
 	echo $dir
 done
 
-echo $BACKUP_DIR
-echo $TIMESTAMP
-echo $NEW_BACKUP_DIR
+check_snapshot_file
+
 create_backup
