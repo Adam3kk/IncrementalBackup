1f108c34e16f58e736f54872ae3c1c681ba2d3d6
Adam <adam.walkowiak028@gmail.com>
2024-06-02 23:09:26 +0200

    Add encryption during archive creation

--- backup.sh
+++ backup.sh
@@ -5,7 +5,7 @@ DIRS=("/home/adam/files" "/test/files") #sciezki do okreslonych folderow
 BACKUP_DIR="/tmp/backup" #sciezka do zapisywania kopii
 TIMESTAMP=$(date +%Y_%m_%d_%H_%M_%S) #aktualna data
 SNAPSHOT_FILE="$BACKUP_DIR/snapshot.file" #plik pamietajacy ostatnie zmiany w kopiach
-
+ENCRYPTION_PASSWORD="qwerty123" #Haslo do szyfrowania
 
 check_snapshot_file(){
 	if [ ! -f "$SNAPSHOT_FILE" ]; then
@@ -19,22 +19,10 @@ check_snapshot_file(){
 #funkcja do tworzenia kopii przyrostowej
 create_backup(){
 	local backup_name="backup_$TIMESTAMP"
-	local temp_backup="$BACKUP_DIR/$backup_name.tar"
+	local temp_backup="$BACKUP_DIR/$backup_name.tar.enc"
 	
-	tar --listed-incremental=$SNAPSHOT_FILE -czf $temp_backup ${DIRS[@]}
+	tar --listed-incremental=$SNAPSHOT_FILE -cz ${DIRS[@]} | openssl enc -aes-256-cbc -pbkdf2 -pass pass:$ENCRYPTION_PASSWORD -out $temp_backup 
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 #wyswietlanie wszystkich folderow
