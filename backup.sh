f3a42a528fdfc2ed030b9b8b4f83ecc42b57dc48
Adam3kk <adam.walkowiak028@gmail.com>
2025-07-22 17:52:32 +0000

    Translate script into English and add function to check if LOG_FILE is defined

--- backup.sh
+++ backup.sh
@@ -8,10 +8,20 @@ DIRS=("${SOURCE_DIRS[@]}")
 exec >> "$LOG_FILE" 2>&1
 echo "========== START BACKUP: $(date) =========="
 
+
+
+check_log_file(){
+	if [ -z "$LOG_FILE" ]; then
+		echo "LOG_FILE path is not included."
+		exit 1
+	fi
+}
+
+
 check_dirs(){
 	for dir in ${DIRS[@]}; do
 		if [ ! -d "$dir" ]; then
-			echo "Podany foldery nie istnieja.. Przerywam wykonywanie kopii"
+			echo "One or more source directories are missing. Stopping backup"
 			exit 1
 		fi
 	done
@@ -26,13 +36,13 @@ check_backup_dir(){
 check_snapshot_file(){
 	if [ ! -f "$SNAPSHOT_FILE" ]; then
 		touch "$SNAPSHOT_FILE"
-		echo "Utworzono plik snapshot.file"
+		echo "Created snaphshot file snapshot.file"
 	else
-		echo "Plik snapshot.file juz istnieje"
+		echo "File snapshot.file already exists"
 	fi
 }
 
-#funkcja do tworzenia kopii przyrostowej
+#Function to create an incremental backup
 create_backup(){
 	
 	TIMESTAMP=$(date +%Y%m%d_%H%M%S)
@@ -44,25 +54,27 @@ create_backup(){
 
 }
 
-#funkcja umozliwiajaca przesylanie kopii
+#Function to send backup files to a remote server
 send_to_server(){
 
         rsync -avz -e "ssh -i ${SSH_KEY}" --progress "${BACKUP_DIR}/" "${DEST_USER}@${DEST_HOST}:${DEST_PATH}/"
 	
 	if [[ $? -eq 0 ]]; then
-		echo "Przesylanie zakonczylo sie sukcesem"
+		echo "Successfully sent all data to the remote server."
 	else	
-		echo "Blad podczas przesylania kopii zapasowej, kod bledu $?."
+		echo "Problem sending data. Error code: $?."
 	fi
 
 
 
 }
 
-#Wywolanie poszczegolnych funkcji
-
+#Execute all functions
 check_dirs
 check_backup_dir
 check_snapshot_file
 create_backup
 send_to_server
+
+echo "========== BACKUP COMPLETED: $(date) =========="
+
